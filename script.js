ce1659066935eeb88c4860aeaa5145ba4bfe3f16
ZoieBoyd <zoieboyd@outlook.com>
2025-01-20 02:06:31 +0000

    Implement basic game functions for console functionality
    - Created two IIFEs to separate game logic: gameboard and gameController.
    - Created a factory function to create new player objects.
    - Implemented a 3x3 empty gameboard using a 2D array.
    - Implemented logic to place a symbol an empty gameboard cell.
    - Added logic to check for a winner after each turn.

--- script.js
+++ script.js
@@ -1,11 +1,86 @@
-const Gameboard = {
-    gameboard: []
+const gameboard = (() => { // IIFE
+    const board = new Array(3).fill(null).map(() => new Array(3).fill("")); // Creates 3x3 2D Array- easier to reference cells in console for testing
+    const getBoard = () => board;
+    return {getBoard};
+})();
+
+function player(name, symbol) { // Factory Function
+    let score = 0;
+    const getScore = () => score;
+    const increaseScore = () => score++;
+    const getPlayerName = () => name;
+    const getPlayerSymbol = () => symbol;
+    return {getPlayerName, getPlayerSymbol, getScore, increaseScore};
 }
 
-const Player = function (name) {
-    this.name = name, 
-    this.score = 0
+const gameController = (() => {
+    const board = gameboard.getBoard();
+    const players = [
+        player("Player One", "X"),
+        player("Player Two", "O")
+    ];
+
+    let currentPlayer = players[0];
+    const switchPlayerTurn = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    }
+    const getCurrentPlayer = () => currentPlayer;
+
+    const makeMove = (row, column) => { 
+        if (board[row][column] === "") {
+            board[row][column] = currentPlayer.getPlayerSymbol();
+            console.log(board);
+            checkWinner();
+            switchPlayerTurn();
+        } else {
+            console.log("Symbol already present in this cell or does not exist! Try again");
+        }
+    }
+
+    const winningConditions = [
+        // Columns
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+        // Rows
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+        // Diagonals
+        [[0, 0], [1, 1], [2, 2]],
+        [[2, 0], [1, 1], [0, 2]]
+    ];
+
+    const checkWinner = () => {
+        const coordinates = [];
+        for(let row = 0; row < board.length; row++) {
+            for(let column = 0; column < board[row].length; column++) {
+                if(currentPlayer.getPlayerSymbol() === board[row][column]) {
+                    coordinates.push([row, column]);
+                }
+            }
+        }
+        for (const condition of winningConditions) {
+            if (
+                condition.every(conditionCoords => 
+                    coordinates.some(playerCoords =>
+                        playerCoords[0] === conditionCoords[0] &&
+                        playerCoords[1] === conditionCoords[1]
+                    )
+                )
+            ) {
+                console.log("Winner");
+            }
+        }
+    }        
+
+    return {switchPlayerTurn, getCurrentPlayer, makeMove}
+})();
+/*
+// This should be the only section that handles DOM elements.
+function screenController() {
+
 }
 
-const Game = {
-}
+screenController();
+*/
